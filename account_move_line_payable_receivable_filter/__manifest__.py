d9cd12076b7f52af56687cefbac389856ca6ff21
Kinner Vachhani <kenvac@users.noreply.github.com>
2018-02-26 09:46:21 +0000

    Revert "[MIG] account_move_line_payable_receivable_filter: Migrated to 10.0"
    This reverts commit 722692969aa1b80bc3de9e14683494b30f33f185.

--- account_move_line_payable_receivable_filter/__manifest__.py
+++ account_move_line_payable_receivable_filter/__manifest__.py
@@ -1,7 +1,27 @@
 # -*- coding: utf-8 -*-
-# © 2015 ACSONE SA/NV (<http://acsone.eu>)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-
+##############################################################################
+#
+#     This file is part of account_move_line_payable_receivable_filter, an
+#     Odoo module.
+#
+#     Copyright (c) 2015 ACSONE SA/NV (<http://acsone.eu>)
+#
+#     account_move_line_payable_receivable_filter is free software: you can
+#     redistribute it and/or modify it under the terms of the GNU Affero
+#     General Public License as published by the Free Software Foundation,
+#     either version 3 of the License, or (at your option) any later version.
+#
+#     account_move_line_payable_receivable_filter is distributed in the hope
+#     that it will be useful, but WITHOUT ANY WARRANTY; without even the
+#     implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#     See the GNU Affero General Public License for more details.
+#
+#     You should have received a copy of the
+#     GNU Affero General Public License
+#     along with account_move_line_payable_receivable_filter.
+#     If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
 {
     'name': "Account Move Line Payable Receivable Filter",
 
@@ -10,7 +30,7 @@
     'author': "ACSONE SA/NV,Odoo Community Association (OCA)",
     'website': "http://acsone.eu",
     'category': 'Accounting & Finance',
-    'version': '10.0.1.0.0',
+    'version': '8.0.1.0.0',
     'license': 'AGPL-3',
     'depends': [
         'account',
@@ -18,5 +38,5 @@
     'data': [
         'views/account_move_line_view.xml',
     ],
-    'installable': True,
+    'installable': False,
 }
