5a0c8971f5fe477e923d2b14475c3d020dceda7f
Kinner Vachhani <kenvac@users.noreply.github.com>
2018-02-26 09:46:21 +0000

    Revert "Add constraint if account type is used on account.account"
    This reverts commit 23fe05d938856f985028a91e6fb47e59b1aca7e0.

--- account_type_inactive/models/account_type.py
+++ account_type_inactive/models/account_type.py
@@ -2,8 +2,7 @@
 # Copyright 2017 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields, api, _
-from odoo.exceptions import ValidationError
+from odoo import models, fields
 
 
 class AccountAccountType(models.Model):
@@ -11,13 +10,3 @@ class AccountAccountType(models.Model):
     _inherit = 'account.account.type'
 
     active = fields.Boolean('Active', default=True)
-
-    @api.constrains('active')
-    def _check_account_moves(self):
-        if not self.active:
-            accounts = self.env['account.account'].search([
-                ('user_type_id', '=', self.id)])
-            if accounts:
-                raise ValidationError(_('You cannot inactive this account type'
-                                        ' as this type is is used on %d '
-                                        'accounts.' % len(accounts)))
