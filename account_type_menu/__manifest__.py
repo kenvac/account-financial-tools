0be28dbab47d7292057c4f820d5946b8bafa993b
Kinner Vachhani <kenvac@users.noreply.github.com>
2018-02-26 09:46:21 +0000

    Revert "Account Type => Account Types in module name"
    This reverts commit b491836c6cd4de18f27320e50c0f6dd373ca736d.

--- account_type_menu/__manifest__.py
+++ account_type_menu/__manifest__.py
@@ -4,11 +4,11 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Account Types Menu',
+    'name': 'Account Type Menu',
     'version': '10.0.1.0.0',
     'category': 'Accounting',
     'license': 'AGPL-3',
-    'summary': "Adds a menu entry for Account Types",
+    'summary': "Adds a menu entry for account types",
     'author': "Akretion,Odoo Community Association (OCA)",
     'website': 'http://www.akretion.com/',
     'depends': ['account'],
