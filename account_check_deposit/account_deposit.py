5bed764d0930f986182ace0b7df97cb70aa33bff
Kinner Vachhani <kenvac@users.noreply.github.com>
2018-02-26 09:46:21 +0000

    Revert "account_check_deposit : Port to new API"
    This reverts commit c260f546d8aa9f0af58fa3da3d10301719a7a9fd.

--- account_check_deposit/account_deposit.py
+++ account_check_deposit/account_deposit.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 ###############################################################################
 #
-#   account_check_deposit for Odoo
+#   account_check_deposit for Odoo/OpenERP
 #   Copyright (C) 2012-2015 Akretion (http://www.akretion.com/)
 #   @author: Benoît GUILLOT <benoit.guillot@akretion.com>
 #   @author: Chafique DELLI <chafique.delli@akretion.com>
@@ -22,24 +22,19 @@
 #
 ###############################################################################
 
-from openerp import models, fields, api, _
+from openerp.osv import fields, orm
+from openerp.tools.translate import _
 import openerp.addons.decimal_precision as dp
-from openerp.exceptions import ValidationError
-from openerp.exceptions import Warning as UserError
 
 
-class AccountCheckDeposit(models.Model):
+class account_check_deposit(orm.Model):
     _name = "account.check.deposit"
     _description = "Account Check Deposit"
     _order = 'deposit_date desc'
 
-    @api.multi
-    @api.depends(
-        'company_id', 'currency_id', 'check_payment_ids.debit',
-        'check_payment_ids.amount_currency',
-        'move_id.line_id.reconcile_id')
-    def _compute_check_deposit(self):
-        for deposit in self:
+    def _compute_check_deposit(self, cr, uid, ids, name, args, context=None):
+        res = {}
+        for deposit in self.browse(cr, uid, ids, context=context):
             total = 0.0
             count = 0
             reconcile = False
@@ -56,70 +51,82 @@ class AccountCheckDeposit(models.Model):
                 for line in deposit.move_id.line_id:
                     if line.debit > 0 and line.reconcile_id:
                         reconcile = True
-            deposit.total_amount = total
-            deposit.is_reconcile = reconcile
-            deposit.currency_none_same_company_id =\
-                currency_none_same_company_id
-            deposit.check_count = count
+            res[deposit.id] = {
+                'total_amount': total,
+                'is_reconcile': reconcile,
+                'currency_none_same_company_id': currency_none_same_company_id,
+                'check_count': count,
+                }
+        return res
 
-    name = fields.Char(string='Name', size=64, readonly=True, default='/')
-    check_payment_ids = fields.One2many(
-        'account.move.line', 'check_deposit_id', string='Check Payments',
-        states={'done': [('readonly', '=', True)]})
-    deposit_date = fields.Date(
-        string='Deposit Date', required=True,
-        states={'done': [('readonly', '=', True)]},
-        default=fields.Date.context_today)
-    journal_id = fields.Many2one(
-        'account.journal', string='Journal', domain=[('type', '=', 'bank')],
-        required=True, states={'done': [('readonly', '=', True)]})
-    journal_default_account_id = fields.Many2one(
-        'account.account', related='journal_id.default_debit_account_id',
-        string='Default Debit Account of the Journal')
-    currency_id = fields.Many2one(
-        'res.currency', string='Currency', required=True,
-        states={'done': [('readonly', '=', True)]})
-    currency_none_same_company_id = fields.Many2one(
-        'res.currency', compute='_compute_check_deposit',
-        string='Currency (False if same as company)')
-    state = fields.Selection([
-        ('draft', 'Draft'),
-        ('done', 'Done'),
-        ], string='Status', default='draft', readonly=True)
-    move_id = fields.Many2one(
-        'account.move', string='Journal Entry', readonly=True)
-    partner_bank_id = fields.Many2one(
-        'res.partner.bank', string='Bank Account', required=True,
-        domain="[('company_id', '=', company_id)]",
-        states={'done': [('readonly', '=', True)]})
-    line_ids = fields.One2many(
-        'account.move.line', related='move_id.line_id',
-        string='Lines', readonly=True)
-    company_id = fields.Many2one(
-        'res.company', string='Company', required=True,
-        states={'done': [('readonly', '=', True)]},
-        default=lambda self: self.env['res.company']._company_default_get(
-            'account.check.deposit'))
-    total_amount = fields.Float(
-        compute='_compute_check_deposit',
-        string="Total Amount", readonly=True,
-        digits=dp.get_precision('Account'))
-    check_count = fields.Integer(
-        compute='_compute_check_deposit', readonly=True,
-        string="Number of Checks")
-    is_reconcile = fields.Boolean(
-        compute='_compute_check_deposit', readonly=True,
-        string="Reconcile")
+    _columns = {
+        'name': fields.char(
+            'Name', size=64, readonly=True),
+        'check_payment_ids': fields.one2many(
+            'account.move.line', 'check_deposit_id', 'Check Payments',
+            states={'done': [('readonly', '=', True)]}),
+        'deposit_date': fields.date(
+            'Deposit Date', required=True,
+            states={'done': [('readonly', '=', True)]}),
+        'journal_id': fields.many2one(
+            'account.journal', 'Journal', domain=[('type', '=', 'bank')],
+            required=True, states={'done': [('readonly', '=', True)]}),
+        'journal_default_account_id': fields.related(
+            'journal_id', 'default_debit_account_id', type='many2one',
+            relation='account.account',
+            string='Default Debit Account of the Journal'),
+        'currency_id': fields.many2one(
+            'res.currency', 'Currency', required=True,
+            states={'done': [('readonly', '=', True)]}),
+        'currency_none_same_company_id': fields.function(
+            _compute_check_deposit, type='many2one',
+            relation='res.currency', multi='deposit',
+            string='Currency (False if same as company)'),
+        'state': fields.selection([
+            ('draft', 'Draft'),
+            ('done', 'Done'),
+            ], 'Status', readonly=True),
+        'move_id': fields.many2one(
+            'account.move', 'Journal Entry', readonly=True),
+        'partner_bank_id': fields.many2one(
+            'res.partner.bank', 'Bank Account', required=True,
+            domain="[('company_id', '=', company_id)]",
+            states={'done': [('readonly', '=', True)]}),
+        'line_ids': fields.related(
+            'move_id', 'line_id', relation='account.move.line',
+            type='one2many', string='Lines', readonly=True),
+        'company_id': fields.many2one(
+            'res.company', 'Company', required=True,
+            change_default=True,
+            states={'done': [('readonly', '=', True)]}),
+        'total_amount': fields.function(
+            _compute_check_deposit, multi='deposit',
+            string="Total Amount", readonly=True,
+            type="float", digits_compute=dp.get_precision('Account')),
+        'check_count': fields.function(
+            _compute_check_deposit, multi='deposit', readonly=True,
+            string="Number of Checks", type="integer"),
+        'is_reconcile': fields.function(
+            _compute_check_deposit, multi='deposit', readonly=True,
+            string="Reconcile", type="boolean"),
+    }
 
-    @api.multi
-    @api.constrains('currency_id', 'check_payment_ids', 'company_id')
-    def _check_deposit(self):
-        for deposit in self:
+    _defaults = {
+        'name': '/',
+        'deposit_date': fields.date.context_today,
+        'state': 'draft',
+        'company_id': lambda self, cr, uid, c: self.pool['res.company'].
+        _company_default_get(cr, uid, 'account.check.deposit', context=c),
+    }
+
+    def _check_deposit(self, cr, uid, ids):
+        for deposit in self.browse(cr, uid, ids):
             deposit_currency = deposit.currency_id
             if deposit_currency == deposit.company_id.currency_id:
                 for line in deposit.check_payment_ids:
                     if line.currency_id:
-                        raise ValidationError(
+                        raise orm.except_orm(
+                            _('Error:'),
                             _("The check with amount %s and reference '%s' "
                                 "is in currency %s but the deposit is in "
                                 "currency %s.") % (
@@ -129,27 +136,35 @@ class AccountCheckDeposit(models.Model):
             else:
                 for line in deposit.check_payment_ids:
                     if line.currency_id != deposit_currency:
-                        raise ValidationError(
+                        raise orm.except_orm(
+                            _('Error:'),
                             _("The check with amount %s and reference '%s' "
                                 "is in currency %s but the deposit is in "
                                 "currency %s.") % (
                                 line.debit, line.ref or '',
                                 line.currency_id.name,
                                 deposit_currency.name))
+        return True
 
-    @api.multi
-    def unlink(self):
-        for deposit in self:
+    _constraints = [(
+        _check_deposit,
+        "All the checks of the deposit must be in the currency of the deposit",
+        ['currency_id', 'check_payment_ids', 'company_id']
+        )]
+
+    def unlink(self, cr, uid, ids, context=None):
+        for deposit in self.browse(cr, uid, ids, context=context):
             if deposit.state == 'done':
-                raise UserError(
+                raise orm.except_orm(
+                    _('Error!'),
                     _("The deposit '%s' is in valid state, so you must "
                         "cancel it before deleting it.")
                     % deposit.name)
-        return super(AccountCheckDeposit, self).unlink()
+        return super(account_check_deposit, self).unlink(
+            cr, uid, ids, context=context)
 
-    @api.multi
-    def backtodraft(self):
-        for deposit in self:
+    def backtodraft(self, cr, uid, ids, context=None):
+        for deposit in self.browse(cr, uid, ids, context=context):
             if deposit.move_id:
                 # It will raise here if journal_id.update_posted = False
                 deposit.move_id.button_cancel()
@@ -160,30 +175,29 @@ class AccountCheckDeposit(models.Model):
             deposit.write({'state': 'draft'})
         return True
 
-    @api.model
-    def create(self, vals):
+    def create(self, cr, uid, vals, context=None):
         if vals.get('name', '/') == '/':
-            vals['name'] = self.env['ir.sequence'].\
-                next_by_code('account.check.deposit')
-        return super(AccountCheckDeposit, self).create(vals)
+            vals['name'] = self.pool['ir.sequence'].\
+                next_by_code(cr, uid, 'account.check.deposit')
+        return super(account_check_deposit, self).\
+            create(cr, uid, vals, context=context)
 
-    @api.model
-    def _prepare_account_move_vals(self, deposit):
+    def _prepare_account_move_vals(self, cr, uid, deposit, context=None):
         date = deposit.deposit_date
-        period_obj = self.env['account.period']
-        period_ids = period_obj.find(dt=date)
+        period_obj = self.pool['account.period']
+        period_ids = period_obj.find(cr, uid, dt=date, context=context)
         # period_ids will always have a value, cf the code of find()
         move_vals = {
             'journal_id': deposit.journal_id.id,
             'date': date,
-            'period_id': period_ids[0].id,
+            'period_id': period_ids[0],
             'name': _('Check Deposit %s') % deposit.name,
             'ref': deposit.name,
             }
         return move_vals
 
-    @api.model
-    def _prepare_move_line_vals(self, line):
+    def _prepare_move_line_vals(
+            self, cr, uid, line, context=None):
         assert (line.debit > 0), 'Debit must have a value'
         return {
             'name': _('Check Deposit - Ref. Check %s') % line.ref,
@@ -195,9 +209,9 @@ class AccountCheckDeposit(models.Model):
             'amount_currency': line.amount_currency * -1,
             }
 
-    @api.model
     def _prepare_counterpart_move_lines_vals(
-            self, deposit, total_debit, total_amount_currency):
+            self, cr, uid, deposit, total_debit, total_amount_currency,
+            context=None):
         return {
             'name': _('Check Deposit %s') % deposit.name,
             'debit': total_debit,
@@ -208,74 +222,119 @@ class AccountCheckDeposit(models.Model):
             'amount_currency': total_amount_currency,
             }
 
-    @api.multi
-    def validate_deposit(self):
-        am_obj = self.env['account.move']
-        aml_obj = self.env['account.move.line']
-        for deposit in self:
-            move_vals = self._prepare_account_move_vals(deposit)
-            move = am_obj.create(move_vals)
+    def validate_deposit(self, cr, uid, ids, context=None):
+        am_obj = self.pool['account.move']
+        aml_obj = self.pool['account.move.line']
+        if context is None:
+            context = {}
+        for deposit in self.browse(cr, uid, ids, context=context):
+            move_vals = self._prepare_account_move_vals(
+                cr, uid, deposit, context=context)
+            context['journal_id'] = move_vals['journal_id']
+            context['period_id'] = move_vals['period_id']
+            move_id = am_obj.create(cr, uid, move_vals, context=context)
             total_debit = 0.0
             total_amount_currency = 0.0
-            to_reconcile_lines = []
+            to_reconcile_line_ids = []
             for line in deposit.check_payment_ids:
                 total_debit += line.debit
                 total_amount_currency += line.amount_currency
-                line_vals = self._prepare_move_line_vals(line)
-                line_vals['move_id'] = move.id
-                move_line = aml_obj.create(line_vals)
-                to_reconcile_lines.append(line + move_line)
+                line_vals = self._prepare_move_line_vals(
+                    cr, uid, line, context=context)
+                line_vals['move_id'] = move_id
+                move_line_id = aml_obj.create(
+                    cr, uid, line_vals, context=context)
+                to_reconcile_line_ids.append([line.id, move_line_id])
 
             # Create counter-part
             if not deposit.company_id.check_deposit_account_id:
-                raise UserError(
+                raise orm.except_orm(
+                    _('Configuration Error:'),
                     _("Missing Account for Check Deposits on the "
                         "company '%s'.") % deposit.company_id.name)
 
             counter_vals = self._prepare_counterpart_move_lines_vals(
-                deposit, total_debit, total_amount_currency)
-            counter_vals['move_id'] = move.id
-            aml_obj.create(counter_vals)
+                cr, uid, deposit, total_debit, total_amount_currency,
+                context=context)
+            counter_vals['move_id'] = move_id
+            aml_obj.create(cr, uid, counter_vals, context=context)
 
-            move.post()
-            deposit.write({'state': 'done', 'move_id': move.id})
+            am_obj.post(cr, uid, [move_id], context=context)
+            deposit.write({'state': 'done', 'move_id': move_id})
             # We have to reconcile after post()
-            for reconcile_lines in to_reconcile_lines:
-                reconcile_lines.reconcile()
+            for reconcile_line_ids in to_reconcile_line_ids:
+                aml_obj.reconcile(
+                    cr, uid, reconcile_line_ids, context=context)
         return True
 
-    @api.onchange('company_id')
-    def onchange_company_id(self):
-        if self.company_id:
-            partner_banks = self.env['res.partner.bank'].search(
-                [('company_id', '=', self.company_id.id)])
-            if len(partner_banks) == 1:
-                self.partner_bank_id = partner_banks[0]
+    def onchange_company_id(
+            self, cr, uid, ids, company_id, currency_id, context=None):
+        vals = {}
+        if company_id:
+            company = self.pool['res.company'].browse(
+                cr, uid, company_id, context=context)
+            if currency_id:
+                if company.currency_id.id == currency_id:
+                    vals['currency_none_same_company_id'] = False
+                else:
+                    vals['currency_none_same_company_id'] = currency_id
+            partner_bank_ids = self.pool['res.partner.bank'].search(
+                cr, uid, [('company_id', '=', company_id)], context=context)
+            if len(partner_bank_ids) == 1:
+                vals['partner_bank_id'] = partner_bank_ids[0]
         else:
-            self.partner_bank_id = False
+            vals['currency_none_same_company_id'] = False
+            vals['partner_bank_id'] = False
+        return {'value': vals}
 
-    @api.onchange('journal_id')
-    def onchange_journal_id(self):
-        if self.journal_id:
-            if self.journal_id.currency:
-                self.currency_id = self.journal_id.currency
+    def onchange_journal_id(self, cr, uid, ids, journal_id, context=None):
+        vals = {}
+        if journal_id:
+            journal = self.pool['account.journal'].browse(
+                cr, uid, journal_id, context=context)
+            vals['journal_default_account_id'] = \
+                journal.default_debit_account_id.id
+            if journal.currency:
+                vals['currency_id'] = journal.currency.id
             else:
-                self.currency_id = self.journal_id.company_id.currency_id
+                vals['currency_id'] = journal.company_id.currency_id.id
+        else:
+            vals['journal_default_account_id'] = False
+        return {'value': vals}
+
+    def onchange_currency_id(
+            self, cr, uid, ids, currency_id, company_id, context=None):
+        vals = {}
+        if currency_id and company_id:
+            company = self.pool['res.company'].browse(
+                cr, uid, company_id, context=context)
+            if company.currency_id.id == currency_id:
+                vals['currency_none_same_company_id'] = False
+            else:
+                vals['currency_none_same_company_id'] = currency_id
+        else:
+            vals['currency_none_same_company_id'] = False
+        return {'value': vals}
 
 
-class AccountMoveLine(models.Model):
+class account_move_line(orm.Model):
     _inherit = "account.move.line"
 
-    check_deposit_id = fields.Many2one(
-        'account.check.deposit', string='Check Deposit', copy=False)
+    _columns = {
+        'check_deposit_id': fields.many2one(
+            'account.check.deposit',
+            'Check Deposit'),
+    }
 
 
-class ResCompany(models.Model):
+class res_company(orm.Model):
     _inherit = 'res.company'
 
-    check_deposit_account_id = fields.Many2one(
-        'account.account', string='Account for Check Deposits', copy=False,
-        domain=[
-            ('type', '<>', 'view'),
-            ('type', '<>', 'closed'),
-            ('reconcile', '=', True)])
+    _columns = {
+        'check_deposit_account_id': fields.many2one(
+            'account.account', 'Account for Check Deposits',
+            domain=[
+                ('type', '<>', 'view'),
+                ('type', '<>', 'closed'),
+                ('reconcile', '=', True)]),
+        }
