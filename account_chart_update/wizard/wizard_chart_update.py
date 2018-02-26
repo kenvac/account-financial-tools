f9794e261f6209ffea7692a1d4df463712f68598
Kinner Vachhani <kenvac@users.noreply.github.com>
2018-02-26 09:46:21 +0000

    Revert "[FIX] account_chart_update: Fetch account code digits correctly"
    This reverts commit a3d8c06b6f1bdc76541e1878324d57f44011155c.

--- account_chart_update/wizard/wizard_chart_update.py
+++ account_chart_update/wizard/wizard_chart_update.py
@@ -154,12 +154,31 @@ class WizardUpdateChartsAccounts(models.TransientModel):
 
     @api.model
     def _get_code_digits(self, company=None):
-        """Returns the number of digits for the accounts, fetched from
-        the company.
+        """Returns the default code size for the accounts.
+        To figure out the number of digits of the accounts it look at the
+        code size of the default receivable account of the company
+        (or user's company if any company is given).
         """
-        if company is None:
+        property_obj = self.env['ir.property']
+        if not company:
             company = self.env.user.company_id
-        return company.accounts_code_digits or 6
+        properties = property_obj.search(
+            [('name', '=', 'property_account_receivable'),
+             ('company_id', '=', company.id),
+             ('res_id', '=', False),
+             ('value_reference', '!=', False)])
+        number_digits = 6
+        if not properties:
+            # Try to get a generic (no-company) property
+            properties = property_obj.search(
+                [('name', '=', 'property_account_receivable'),
+                 ('res_id', '=', False),
+                 ('value_reference', '!=', False)])
+        if properties:
+            account = property_obj.get_by_record(properties[0])
+            if account:
+                number_digits = len(account.code)
+        return number_digits
 
     @api.onchange('company_id')
     def onchange_company_id(self):
